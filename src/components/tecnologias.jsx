fb867fd060e32376a5f69c029f16815f4c20f4db
vinicius <viniciusmouraaragao@hotmail.com>
2021-05-04 17:05:17 -0300

    refactor: using state and removed useless components

--- src/components/tecnologias.jsx
+++ src/components/tecnologias.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import Texto from './apresentacao'
-import Javascript from "./techs/javascript"
-import Nodejs from './techs/nodejs'
-import Reactjs from './techs/reactjs'
-import Java from './techs/java'
-import Git from './techs/git'
-import Shell from './techs/shell'
-import Python from './techs/python'
-import Typescript from './techs/typescript'
-import Kotlin from './techs/kotlin'
-import PHP from './techs/php'
+import Tecs from "./techs";
+import logos from '../logos.json'
+import textos from '../textos.json'
+
 import '../styles/tecnologias.css'
 
    function Tecnologias() {
@@ -20,17 +14,56 @@ import '../styles/tecnologias.css'
             <Texto subtitulo="De maior proficiência até menor proficiência" />
         <div className="teste">
             <ul className="tabelaDeTecnologias">
-                <li> <Javascript /> </li>
-                <li> <Nodejs /> </li>
-                <li> <Reactjs /> </li>
-                <li> <Java /> </li>
-                <li> <Git /> </li>
-                <li><Shell /></li>
-                <li><Python /></li>
-                <li><Typescript /></li>
-                <li><Kotlin /></li>
-                <li><PHP /></li>
-                <li>CSS3</li>
+                <li> 
+                    <Tecs 
+                    nome="Javascript"
+                    logo={ logos.javascript }
+                    texto={ textos.javascript } /></li>
+                <li> 
+                    <Tecs 
+                    nome="React"
+                    logo={ logos.react }
+                    texto={ textos.react } /></li>
+                <li>
+                    <Tecs 
+                    nome="React Native" 
+                    logo={ logos.native }
+                    texto={ textos.native } /></li>
+                <li> 
+                    <Tecs 
+                    nome="Nodejs"
+                    logo={ logos.nodejs }
+                    texto={ textos.nodejs } /></li>
+                <li> 
+                    <Tecs 
+                    nome="Java" 
+                    logo={logos.java} 
+                    texto={textos.java} /></li>
+                <li>
+                    <Tecs 
+                    nome="Typescript" 
+                    logo={ logos.typescript } 
+                    texto={ textos.typescript } /></li>
+                <li> 
+                    <Tecs 
+                    nome="Git" 
+                    logo={ logos.git} 
+                    texto={textos.git} /></li>
+                <li>
+                    <Tecs
+                    nome="Python"
+                    logo={ logos.python }
+                    texto={ textos.python } /></li>
+                <li>
+                    <Tecs 
+                    nome='Kotlin' 
+                    logo={ logos.kotlin } 
+                    texto={ textos.kotlin } /></li>
+                <li>
+                    <Tecs 
+                    nome='PHP' 
+                    logo={ logos.php } 
+                    texto={ textos.php } /></li>
             </ul>
         </div>
     </>
